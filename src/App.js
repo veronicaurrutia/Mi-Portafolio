ce37e43b7f23f7c853ea40c5680ce62225e92de6
Vero <vero.urrutiaa@gmail.com>
2025-01-21 18:25:40 -0300

    se elimina router es5

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import './index'
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from './Components/Home'
@@ -11,7 +10,6 @@ import Skills from './Components/Skills'
 
 function App() {
   return (
- <Router basename="/Portafolio-React">
   <Routes>
   <Route path="/" element={<Home />} />
   <Route path="/Navbar" element={<Navbar />} />
@@ -19,9 +17,7 @@ function App() {
   <Route path="/contact" element={<Contact />} />
   <Route path="/Skills" element={<Skills />} />
   <Route path="/Projects" element={<Projects />} />
-
  </Routes>
- </Router>
   );
 }
 
